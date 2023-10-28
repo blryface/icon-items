67cb6d596b4397fceb03cd7a9926fb5bd0dd959b
blryface <124838505+blryface@users.noreply.github.com>
2023-10-28 19:01:11 -0300

    Fix tab

--- src/main/java/xyz/blurryface/iconitems/Iconitems.java
+++ src/main/java/xyz/blurryface/iconitems/Iconitems.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import xyz.blurryface.iconitems.item.ModItems;
 
 public class Iconitems implements ModInitializer {
-    public static final String MOD_ID = "iconitems";
+	public static final String MOD_ID = "iconitems";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override

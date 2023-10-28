9875f6daed48c20c3d5864d6b05246d350609ccd
blryface <124838505+blryface@users.noreply.github.com>
2023-10-27 23:16:44 -0300

    Initial changes

---
+++ src/main/java/xyz/blurryface/iconitems/item/ModItems.java
@@ -0,0 +1,13 @@
+package xyz.blurryface.iconitems.item;
+
+import xyz.blurryface.iconitems.Iconitems;
+
+public class ModItems {
+
+
+    //private static Item
+    public static void registerModItems() {
+        Iconitems.LOGGER.info("Making items real :3");
+    }
+
+}
